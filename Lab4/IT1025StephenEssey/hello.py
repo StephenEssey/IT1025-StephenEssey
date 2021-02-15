facd8642ef37886b1e87cd6f043c3a5c8d8da4cd
StephenEssey <77950783+StephenEssey@users.noreply.github.com>
2021-02-14 23:37:32 -0500

    Add files via upload

---
+++ Lab4/IT1025StephenEssey/hello.py
@@ -0,0 +1,12 @@
+#Stephen Essey, 02/11/2021, code by Automate the Boring Stuff with Python
+
+print('Hello world')
+print('What is your name?') # ask for their name
+myName=input()
+print('It is good to meet you,'+myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?') # ask for their age
+myAge=input()
+print('You will be '+str(int(myAge)+1)+' in a year.')
+x=input()
